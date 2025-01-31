730ac7cdc2f77dc9debe1e9098c9138fef0fe498
keshav2999 <keshv.thahur2978@gmail.com>
2025-01-31 14:35:16 +0530

    created basic file

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
+if __name__ == "_main_":
+    app.run()
